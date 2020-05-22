2ea9d902cf4d8f9e9b4f28d185a11202b6f9baae
Alexandre Bartz <alex.bartz@outlook.fr>
2020-05-22 18:41:06 +0200

    Normalize token

--- level2to3.py
+++ level2to3.py
@@ -1,4 +1,5 @@
 from lxml import etree
+import json
 from pie_extended.cli.sub import get_tagger, get_model
 from pie_extended.models.fr.imports import get_iterator_and_processor
 import os
@@ -8,11 +9,14 @@ import subprocess
 import argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("file", help="file to process")
+arg_parser.add_argument("dict", help="dictionnary used for normalisation")
 arg_parser.add_argument("-l", "--lemmatize", help="used to lemmatize a text", action="store_true")
 arg_parser.add_argument("-n", "--normalize", help="used to normalize a text", action="store_true")
 args = arg_parser.parse_args()
 
 ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
+
+# SEGMENTATION ################################################################
 
 def transform_text(doc):
     """
@@ -71,6 +75,8 @@ def segment_document(doc):
     :rtype: a new XLM doc
     """
     # Only the text enclosed between <p> and <l> is segmented.
+    with open(args.dict, 'r') as f:
+        dico = json.loads(f.read()) 
     paragraphs = doc.xpath('//tei:text//tei:p', namespaces=ns)
     lines = doc.xpath('//tei:text//tei:l', namespaces=ns)
     segment_elements(paragraphs)
@@ -78,7 +84,7 @@ def segment_document(doc):
     # Used to normalize the text.
     if args.normalize:
         segs = doc.xpath('//tei:seg', namespaces=ns)
-        normalize(segs)
+        normalize_segs(segs)
     # Used to lemmatize the text.
     if args.lemmatize:
         if args.normalize:
@@ -88,13 +94,106 @@ def segment_document(doc):
             # If the text is only to be lemmatized, segs are lemmatized.
             tags = doc.xpath('//tei:seg', namespaces=ns)
         lemmatize(tags)
+        for tag in tags:
+            words = tag.xpath('./tei:w', namespaces=ns)
+            normalize_w(words, dico)
     # Indent() inserts tail whitespace for pretty-printing an XML tree.
     etree.indent(doc)
     # This output file is specific to the project e-ditiones, you can easily change the output with e.g. doc.write("New" + args.file, ...)
     return doc.write(args.file.replace("level-2", "level-3"), pretty_print=True, encoding="utf-8", method="xml")
 
 
-def normalize(segs):
+# NORMALIZATION - W ###########################################################
+
+def create_gram(pos, msd):
+    gram = {}
+    infos = msd.split("|")
+    for info in infos:
+        [key, val] = info.split("=")
+        if key == "NOMB.":
+            if val == "s":
+                gram["number"] = "singular"
+            elif val == "p":
+                gram["number"] = "plural"
+        elif key == "GENRE":
+            if val == "f":
+                gram["gen"] = "feminine"
+            elif val == "m":
+                gram ["gen"] = "masculine"
+        elif key == "PERS." :
+            if val == "1":
+                gram["per"] = "firstPerson"
+            elif val == "2":
+                gram["per"] = "secondPerson"
+            elif val == "3":
+                gram["per"] = "thirdPerson"
+        elif key == "TEMPS":
+            if val == "pst":
+                gram["tns"] = "present"
+            elif val == "fut":
+                gram["tns"] = "future"
+            elif val == "ipf":
+                gram["tns"] = "imperfect"
+            elif val == "psp":
+                gram["tns"] = "simplePast"
+        elif key == "MODE":
+            if val == "ind":
+                gram["mood"] = "indicative"
+            elif val == "con":
+                gram["mood"] = "conditional"
+            elif val == "imp":
+                gram["mood"] = "imperative"
+            elif val == "sub":
+                gram["mood"] = "subjonctive"
+    if pos == "VERppe":
+        gram["mood"] = "participle"
+        gram["tns"] = "past"
+    elif pos == "VERppa":
+        gram["mood"] = "participle"
+        gram["tns"] = "present"
+    elif pos == "VERinf":
+        gram["mood"] = "infinitive"
+    return gram
+
+def match_gram(gram, dict_gram):
+    for key in gram:
+        if key not in dict_gram:
+            return False
+        if gram[key] != dict_gram[key] and dict_gram[key] != "invariable":
+            return False
+    return True
+
+
+def normalize_word(word, dico):
+    result = []
+    lemma = word.get("lemma")
+    pos = word.get("pos")
+    msd = word.get("msd")
+    entry = dico.get(lemma)
+    if not entry:
+        return word.text
+    gram = create_gram(pos, msd)
+    for inflected in entry["inflected"]:
+        if "gramGrp" in inflected and match_gram(gram, inflected["gramGrp"]):
+            result.append(inflected["orth"])
+    if len(result) > 0:
+        return "|".join(result)
+    return word.text
+
+
+def normalize_w(words, dico):
+    # Cette fonction permet de normaliser chaque mot en utilisant le lemmatiseur
+    for word in words:
+        orig = etree.Element("{http://www.tei-c.org/ns/1.0}orig")
+        reg = etree.Element("{http://www.tei-c.org/ns/1.0}reg")
+        orig.text = word.text
+        reg.text = normalize_word(word, dico)
+        word.text = ""
+        word.extend([orig, reg])
+
+# NORMALIZATION - SEG #########################################################
+
+def normalize_segs(segs):
     """
     This function process a normalization of the text.
 
@@ -126,6 +225,8 @@ def normalize(segs):
         os.remove(file.name + ".tok_into_fr.test.beam10")
         file.close()
 
+
+# LEMMATIZATION ###############################################################
 
 def lemmatize(segs):
     """
