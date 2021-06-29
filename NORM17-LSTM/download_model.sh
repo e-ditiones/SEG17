e045bd6392b9e298a2528f7c43492813c01b8e7d
Rachel Bawden <rachel.bawden@inria.fr@users.noreply.github.com>
2021-06-29 12:17:43 +0200

    allow for model download

---
+++ NORM17-LSTM/download_model.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# make sure to unpack in the current directory
+thisdir=`realpath $(dirname $0)`
+cd $thisdir
+
+wget http://almanach.inria.fr/files/modfr_norm/lstm_best/model.tar.gz
+tar -xzvf model.tar.gz
+
